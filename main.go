1abd84f04dd5b114787f0a6b6359b0e6303005cd
SamyDnx <doneux.samy.uni@gmail.com>
2025-10-30 01:57:44 +0100

    feat: basic typing text with colors syntax

--- main.go
+++ main.go
@@ -1,79 +1,27 @@
-// !!! Linux only (TCGETS/TCSETS)
-
 package main
 
 import (
 	"fmt"
-	"os"
-	"syscall"
-	"unsafe"
 )
 
-// terminal attributes struct (c.f. man pages)
-type termios struct {
-	Iflag  uint32    // input modes
-	Oflag  uint32    // output modes
-	Cflag  uint32    // control modes
-	Lflag  uint32    // local modes
-	Cc     [32]uint8 // special modes
-	Ispeed uint32    // baud input speed
-	Ospeed uint32    // baud output speed
+// store user progress
+type UserTextProgress struct {
+	progress [256]int8
+	idx      int
 }
 
-// enableRawMode switches the terminal to raw mode and returns the original state
-func enableRawMode() (*termios, error) {
-	fd := int(syscall.Stdin)
-	var oldState termios
-
-	// get current terminal attributes
-	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
-		uintptr(syscall.TCGETS), uintptr(unsafe.Pointer(&oldState)))
-	if errno != 0 {
-		return nil, errno
-	}
-
-	// modify attributes to enable raw mode
-	newState := oldState
-	// disble canonical mode and echo
-	newState.Lflag &^= syscall.ICANON | syscall.ECHO
-
-	// set the new terminal attricutes
-	_, _, errno = syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
-		uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(&newState)))
-	if errno != 0 {
-		return nil, errno
-	}
-
-	return &oldState, nil
-}
-
-// disableRawMode recover the terminal to it's original state
-func disableRawMode(oldState *termios) error {
-	fd := int(syscall.Stdin)
-	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
-		uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(oldState)))
-	if errno != 0 {
-		return errno
-	}
-
-	return nil
-}
-
-// readInput reads a keypress in raw mode
-func readInput() ([]byte, int) {
-	buf := make([]byte, 3)
-
-	n, err := os.Stdin.Read(buf[:])
-	if err != nil {
-		fmt.Printf("Error reading the key: %v\n", err)
-		return nil, -1
-	}
-
-	return buf, n
-}
+// ansi code for colors and reset
+const Red string = "\033[31m"
+const Green string = "\033[32m"
+const Neural string = "\033[37m"
+const Reset string = "\033[2K"
 
 func main() {
-	fmt.Println("Press any key (CTRL+D to exit):")
+	text := []rune("The quick brown fox jumps over the lazy dog")
+	user := UserTextProgress{
+		progress: [256]int8{},
+		idx:      0,
+	}
 
 	oldState, err := enableRawMode()
 	if err != nil {
@@ -82,16 +30,49 @@ func main() {
 	}
 	defer disableRawMode(oldState)
 
+	// 1st text print
+	for _, r := range text {
+		fmt.Printf("%s", string(r))
+	}
 	for {
+		if user.idx >= len(text) {
+			disableRawMode(oldState)
+			fmt.Println("\nTest finished !")
+			return
+		}
+
 		buf, n := readInput()
+
+		// check typed letter
 		if n > 0 {
-			fmt.Printf("Captured: %v (bytes: %v)\n", string(buf[:n]), buf[:n])
+			if buf[0] == byte(text[user.idx]) {
+				user.progress[user.idx] = 1
+			} else {
+				user.progress[user.idx] = -1
+			}
 
 			// extiting with CTRL+D (4)
 			if buf[0] == 4 {
 				fmt.Println("Exiting...")
 				return
 			}
+		}
+		renderText(&user, text)
+		user.idx += 1
+	}
+}
+
+// render text with according colors
+func renderText(user *UserTextProgress, text []rune) {
+	// clear line and put the cursor at the start of line
+	fmt.Printf("%s\r", Reset)
+	for i, r := range text {
+		if user.progress[i] > 0 {
+			fmt.Printf("%s%s%s", Green, string(r), Neural)
+		} else if user.progress[i] < 0 {
+			fmt.Printf("%s%s%s", Red, string(r), Neural)
+		} else {
+			fmt.Printf("%s", string(r))
 		}
 	}
 }
---
+++ rawMode.go
@@ -0,0 +1,73 @@
+// !!! Linux only (TCGETS/TCSETS)
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+// terminal attributes struct (c.f. man pages)
+type termios struct {
+	Iflag  uint32    // input modes
+	Oflag  uint32    // output modes
+	Cflag  uint32    // control modes
+	Lflag  uint32    // local modes
+	Cc     [32]uint8 // special modes
+	Ispeed uint32    // baud input speed
+	Ospeed uint32    // baud output speed
+}
+
+// enableRawMode switches the terminal to raw mode and returns the original state
+func enableRawMode() (*termios, error) {
+	fd := int(syscall.Stdin)
+	var oldState termios
+
+	// get current terminal attributes
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
+		uintptr(syscall.TCGETS), uintptr(unsafe.Pointer(&oldState)))
+	if errno != 0 {
+		return nil, errno
+	}
+
+	// modify attributes to enable raw mode
+	newState := oldState
+	// disble canonical mode and echo
+	newState.Lflag &^= syscall.ICANON | syscall.ECHO
+
+	// set the new terminal attricutes
+	_, _, errno = syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
+		uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(&newState)))
+	if errno != 0 {
+		return nil, errno
+	}
+
+	return &oldState, nil
+}
+
+// disableRawMode recover the terminal to it's original state
+func disableRawMode(oldState *termios) error {
+	fd := int(syscall.Stdin)
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
+		uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(oldState)))
+	if errno != 0 {
+		return errno
+	}
+
+	return nil
+}
+
+// readInput reads a keypress in raw mode
+func readInput() ([]byte, int) {
+	buf := make([]byte, 3)
+
+	n, err := os.Stdin.Read(buf[:])
+	if err != nil {
+		fmt.Printf("Error reading the key: %v\n", err)
+		return nil, -1
+	}
+
+	return buf, n
+}
